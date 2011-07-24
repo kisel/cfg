112361b297479219ec35cc42737473d4ac9468a6
Anton <kiton@ledas.com>
2011-07-24 13:03:24 +0700

    relink improved

--- relink.sh
+++ relink.sh
@@ -3,5 +3,14 @@
 #create links for files
 
 cd ~/.cfg
-cp $1 $2 $3 $4 $5 -l .* ~/ -v
+  
+for i in `find . -type f -level 0 -name ".*"|cut -c 3-`; do
+  echo diff $i ~/$i
+  diff $i ~/$i
+  rc=$?
+
+  if [ "$rc" != "0" ]; then
+    echo "Rewrite?[y/n]" && read ans && [ "$ans" = "y" ] && ln -v -f $i ~/$i
+  fi
+done
 
