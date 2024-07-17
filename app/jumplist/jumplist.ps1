83d647a381b60287b2ebbf43c2b056c094a95cf6
Anton Kiselev <anton.kisel@gmail.com>
2024-07-18 01:01:45 +0700

    jump list add cleanup fun

--- app/jumplist/jumplist.ps1
+++ app/jumplist/jumplist.ps1
@@ -11,3 +11,20 @@ Function jbookmark {
 Function j {
 	cd $(Get-Content $JUMPLIST | fzf --preview="dir {}")
 }
+
+Function jbookmark-remove-deleted {
+    $fileList = Get-Content -Path $JUMPLIST
+    $validFiles = @()
+
+    foreach ($filePath in $fileList) {
+        if (Test-Path ($filePath.Trim())) {
+            $validFiles += $filePath.Trim()
+        } else {
+            Write-Warning "removed outdated entity '$filePath'"
+        }
+        $validFiles | Out-File -FilePath "$JUMPLIST.new" -Append -NoClobber
+        Move-Item -Force $JUMPLIST "$JUMPLIST.bak"
+        Move-Item -Force "$JUMPLIST.new" $JUMPLIST
+    }
+}
+
