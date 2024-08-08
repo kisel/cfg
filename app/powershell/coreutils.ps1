f8b2479a288ced295c033e71d94e4ec0fcb1b931
Anton Kiselev <anton.kisel@gmail.com>
2024-08-08 21:06:27 +0700

    coreutils.ps1: grep

--- app/powershell/coreutils.ps1
+++ app/powershell/coreutils.ps1
@@ -1,11 +1,14 @@
 # Remove standard powershell aliases to use real commands from uutils-coreutils 
 #
-# scoop install uutils-coreutils 
-# scoop info uutils-coreutils 
+# scoop info uutils-coreutils
+#
+# scoop install uutils-coreutils
+# scoop install grep
 #
 remove-alias cp -ErrorAction SilentlyContinue
 remove-alias mv -ErrorAction SilentlyContinue
 remove-alias rm -ErrorAction SilentlyContinue
 remove-alias tee -f -ErrorAction SilentlyContinue
 remove-alias tail -ErrorAction SilentlyContinue
+remove-alias grep -ErrorAction SilentlyContinue
 
