1c63ea818ae80b8b79e205097a6eeb959526c80c
Anton Kiselev <anton.kisel@gmail.com>
2024-10-10 11:56:35 +0700

    add terminal-utils.ps1

---
+++ install/windows/terminal-utils.ps1
@@ -0,0 +1,14 @@
+# linux command line tools
+scoop install uutils-coreutils
+scoop install grep
+# busybox-lean is a single busybox binary, to not conflict with coreutils
+scoop install busybox-lean
+
+# better ls for windows
+scoop install eza
+
+# find
+scoop install fzf fd
+
+# better grep
+scoop install ripgrep
