0a03caab383e3b0a3fd0eeae5e0c05f8bcea59df
Anton Kiselev <anton.kiselev@xored.com>
2024-10-23 20:50:41 +0700

    update git config: tabs, modeline

---
+++ app/git/install/windows-tweaks.ps1
@@ -0,0 +1,8 @@
+# use Windows OpenSSH service agent
+# needs to be enabled in services.msc
+ git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
+
+ git config --global core.longpaths true
+
+ git config --global core.quotepath false
+
