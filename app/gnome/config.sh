bf2741317796d4247f02df8fa42f2e57075ffd4c
Anton Kiselev <anton.kisel@gmail.com>
2026-05-23 13:41:13 +0700

    new file:   app/gnome/config.sh

---
+++ app/gnome/config.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+
+# too many mounts (custom + k8). keep the ui clean
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+
+#[org/gnome/desktop/interface]
+#color-scheme='prefer-dark'
+#enable-animations=false
+#
+#[org/gnome/shell/extensions/just-perfection]
+#accessibility-menu=true
+#animation=0
+#background-menu=true
+#clock-menu=true
+#clock-menu-position=1
+#
